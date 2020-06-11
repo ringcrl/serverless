25ca2363ed674e09d8499b2e311d17f8a704aaf5
ringcrl <ringcrl@foxmail.com>
2020-06-11 23:30:53 +0800

    feat(sls): /translate 句子翻译路由

--- src/sls.js
+++ src/sls.js
@@ -4,46 +4,45 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const app = express()
 
-const secretId = process.env.SID
-const secretKey = process.env.SKEY
+if (!process.env.TENCENT_SECRET_ID) {
+  require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+}
+
+const { TENCENT_SECRET_ID, TENCENT_SECRET_KEY } = process.env
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
-  // Routes
-  
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
+
+app.get('/', (req, res) => {
+  res.send('serverless-translate server')
+})
+
+app.get('/translate', urlencodedParser, async (req, res) => {
+  const text = req.query.text
+
+  const client = new Capi({
+    Region: 'ap-beijing',
+    SecretId: TENCENT_SECRET_ID,
+    SecretKey: TENCENT_SECRET_KEY,
+    ServiceType: 'tmt',
+    host: 'tmt.tencentcloudapi.com',
   })
 
-  app.post('/process', urlencodedParser, async (req, res) => {
-    // 输出 JSON 格式
-    var response = {
-        "target":req.body.target,
-        "words":req.body.words
-    };
-    const client = new Capi({
-      Region: 'ap-beijing',
-      SecretId: secretId,
-      SecretKey: secretKey,
-      ServiceType: 'tmt',
+  const resq = await client.request(
+    {
+      Action: 'TextTranslate',
+      Version: '2018-03-21',
+      SourceText: text,
+      Source: 'auto',
+      Target: 'zh',
+      ProjectId: 0,
+    },
+    {
       host: 'tmt.tencentcloudapi.com',
-    })
+    },
+  )
+  res.send(resq.Response.TargetText)
+})
 
-    const resq = await client.request(
-      {
-        Action: 'TextTranslate',
-        Version: '2018-03-21',
-        SourceText: req.body.words,
-        Source: 'auto',
-        Target: req.body.target,
-        ProjectId: 0,
-      },
-      {
-        host: 'tmt.tencentcloudapi.com',
-      },
-    )
-    res.send( `Your Translation Result : `+ req.body.words + ` ---> ` + resq.Response.TargetText)
-  })
+app.listen(8080)
 
-  app.listen(8080)
-
-  module.exports = app
+module.exports = app
