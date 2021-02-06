c33ac8b0abb2122caf42e1f1b1b8a05f4d94f04d
ringcrl <ringcrl@foxmail.com>
2021-02-06 10:20:47 +0800

    feat: update scheme

--- handler/scheme.js
+++ handler/scheme.js
@@ -1,11 +1,26 @@
+function noop() {}
+
 async function handler(req, res) {
   try {
     let { schemeId } = req.params;
 
-    if (schemeId === 'WeRead' && !req.useragent.isMobile) {
-      res.status = 302;
-      res.redirect('https://weread.qq.com/');
-      return;
+    if (!req.useragent.isMobile) {
+      switch (schemeId) {
+        case 'WeRead':
+          res.status = 302;
+          res.redirect('https://weread.qq.com/');
+          return;
+        case 'bilibili':
+          res.status = 302;
+          res.redirect('https://www.bilibili.com/');
+          return;
+        case 'youtube':
+          res.status = 302;
+          res.redirect('https://www.youtube.com/');
+          return;
+        default:
+          noop();
+      }
     }
 
     if (!schemeId.endsWith('://')) {
